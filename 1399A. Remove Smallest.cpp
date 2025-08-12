70650c2993eef53b19bcc153f4d9c94ccff6a61c
Sanskar-Awachar-commits <awacharsanskar@gmail.com>
2025-08-12 10:35:48 +0530

    1399A. Remove Smallest

---
+++ 1399A. Remove Smallest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int cn = 1; cn <= t; cn++) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i + 1] - a[i] > 1) {
+                cout << "NO" << endl;
+                return;
+            }
+        }
+        cout << "YES" << endl;
+    }
+    return 0;
+}
