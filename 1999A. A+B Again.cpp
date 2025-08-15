2d982ce83043927ebefbc6e8e1dfb804952aad31
Sanskar-Awachar-commits <awacharsanskar@gmail.com>
2025-08-15 09:57:39 +0530

    1999A. A+B Again

---
+++ 1999A. A+B Again.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int a = n % 10;
+        int output = (n - a) / 10 + a;
+        cout << output << endl;
+    }
+    return 0;
+}
