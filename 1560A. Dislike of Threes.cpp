0e7f97ebcfdc4b458212c8736b36ceb5027263f6
Sanskar-Awachar-commits <awacharsanskar@gmail.com>
2025-08-22 15:55:17 +0530

    1560A. Dislike of Threes

---
+++ 1560A. Dislike of Threes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        int i = 0, j = 0;
+        while (j != k) {
+            i++;
+            if (i % 3 != 0 && i % 10 != 3) j++;
+        }
+        cout << i << "\n";
+    }
+    return 0;
+}
